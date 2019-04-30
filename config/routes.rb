eed39ec8f2cea7b90e500392276d52b17909b6fa
M.Onishi <omccello@gmail.com>
2019-05-01 08:39:34 +0900

    Fix related Facebook-login 4(practice delete)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,17 @@ Rails.application.routes.draw do
   get '/tos',       to: 'static_pages#tos'
 
   #課題：deviseをよりRESTfulにする（path）。
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, skip: [:registrations, :sessions]
+  as :user do
+    get 'sign_up',          to: 'devise/registrations#new', as: :new_user_registration
+    post'sign_up',          to: 'devise/registrations#create', as: :user_registration
+    get 'users/:id/edit',   to: 'devise/registrations#edit', as: :edit_user_registration
+    put 'users/:id/edit',   to: 'devise/registrations#update', as: :update_user_registration
+    delete 'users/:id/edit',to: 'devise/registrations#destroy'
+    get 'sign_in',          to: 'devise/sessions#new', as: :new_user_session
+    post 'sign_in',         to: 'devise/sessions#create', as: :user_session
+    match 'sign_out',       to: 'devise/sessions#destroy', as: :destroy_user_session, via: Devise.mappings[:user].sign_out_via
+  end
 
   resources :users, only: [:show, :following, :followers] do
     member do
